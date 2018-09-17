d163714878e4e8af6e6c79256948df837762dbf6
Amogh Lele <amolele@gmail.com>
2018-09-17 13:06:02 +0530

    Destroy the RTCEngine when the activity is destroyed

--- app/src/main/java/org/firehound/devfest/MainActivity.java
+++ app/src/main/java/org/firehound/devfest/MainActivity.java
@@ -1,24 +1,16 @@
 package org.firehound.devfest;
 
-import android.app.ProgressDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Surface;
-import android.view.SurfaceView;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.RadioButton;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 import io.agora.rtc.Constants;
 import io.agora.rtc.IRtcEngineEventHandler;
 import io.agora.rtc.RtcEngine;
-import io.agora.rtc.video.VideoCanvas;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = "MainActivity";
@@ -106,5 +98,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void onDisconnectClicked(View view) {
         rtcEngine.leaveChannel();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        RtcEngine.destroy();
+        rtcEngine = null;
     }
 }
